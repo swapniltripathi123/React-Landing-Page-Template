4fce357a5aba9189f16c13746dfe59573a798a1a
Swapnil Tripathi <57709091+swapniltripathi123@users.noreply.github.com>
2021-06-16 20:50:13 +0530

    Update services.jsx

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -4,51 +4,44 @@ import Environment from '../pages/Environment';
 import './services.css';
 export const Services = (props) => {
   return (
-      <div id="services" className="text-center">
-        <div className="container">
-          <div className="section-title">
-            <h2>Our Services</h2>
-            
-            <nav className="service-nav">
-                <ul>
-                    <li >
-                        <Link to='/engineering' >Engineering Geophysics</Link>
-                    </li>
-                    <li>
-                        <Link to='/environment'>Environment Geophysics</Link>
-                    </li>
-                </ul>
-            </nav>
-            {/* <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-              dapibus leonec.
-            </p> */}
-          </div>
-
-          
-
-
-
-          <div className="row">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div key={`${d.name}-${i}`} className="col-md-4">
-                    {" "}
-                    <i className={d.icon}></i>
-                    <div className="service-desc">
-                      <h3>{d.name}</h3>
-                      <p>{d.text}</p>
-                    </div>
-                  </div>
-                ))
-              : "loading"}
-          </div>
+<<<<<<< users/akde/AddCss
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Our Services</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
         </div>
-        <Switch>
-           <Route path='/engineering' component={Engineering} />
-           <Route path='/environment' component={Environment} />
-        </Switch>
-      </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`}>
+                  <div className='portfolio-items'>
+                    <div className='col-sm-6 col-md-4 col-lg-4'>
+                      <div className='portfolio-item'>
+                        <div className='hover-bg'>
+                          <a href="https://www.google.com"  target="_blank">
+                            <div className='hover-text'>
+                              <h4>{d.name}</h4>
+                            </div>
+                            </a>
+                            <img
+                              src={d.icon}
+                              className='img-responsive'
+                              alt='Project Title'
+                            />
+                        </div>{' '}
+                      </div>
+                    </div>
+                    </div>
+                    </div>
+              ))
+            : 'loading'}
+            
+
       
   );
 };
+>>>>>>> master
